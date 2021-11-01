4eca98708f6f3db6c7658aeb12cefdaee01e46fa
stepanovartem93 <stepanovartem93@gmail.com>
2021-11-01 09:14:02 +0300

    - добавил возможность выводить на сайте информацию, генерируемую на сайт

--- password_generator/generator/views.py
+++ password_generator/generator/views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
-    return render(request, 'generator/home.html')
+    return render(request, 'generator/home.html', {'password':'wrthth2w!'})
 
 def eggs(request):
     return HttpResponse("<h1>Eggs are so tasty<h1>")
